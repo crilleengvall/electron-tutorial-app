ab8526b4e5195b6c38c5c28e44b20661e6f87d46
Christian Engvall <crilleengvall@gmail.com>
2016-08-24 16:06:29 +0200

    adds hyperspace design

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1281, height: 800, minWidth: 1281, minHeight: 800})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
