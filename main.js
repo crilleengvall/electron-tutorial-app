dcd2c0b7477c1af3c07fc8fcfb3fa084a0837f7f
Christian Engvall <crilleengvall@gmail.com>
2016-10-18 13:18:25 +0200

    Adding path join to make it work on linux ubuntu

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+var path = require('path')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -17,7 +18,7 @@ function createWindow () {
     minHeight: 800,
     backgroundColor: '#312450',
     show: false,
-    icon: 'assets/icons/png/1024x1024.png'
+    icon: path.join(__dirname, 'assets/icons/png/1024x1024.png')
   })
 
 
