136f855d831614e5d3daadc13ba164024ba75687
Christian Engvall <crilleengvall@gmail.com>
2017-10-13 11:29:28 +0200

    Using appname when packaging and creating installer for windows

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -14,11 +14,11 @@ function getInstallerConfig () {
   const outPath = path.join(rootPath, 'release-builds')
 
   return Promise.resolve({
-    appDirectory: path.join(outPath, 'Electron\ tutorial\ app-win32-ia32/'),
+    appDirectory: path.join(outPath, 'Electron-tutorial-app-win32-ia32/'),
     authors: 'Christian Engvall',
     noMsi: true,
     outputDirectory: path.join(outPath, 'windows-installer'),
-    exe: 'Electron tutorial app.exe',
+    exe: 'electron-tutorial-app.exe',
     setupExe: 'ElectronTutorialAppInstaller.exe',
     setupIcon: path.join(rootPath, 'assets', 'icons', 'win', 'icon.ico')
   })
